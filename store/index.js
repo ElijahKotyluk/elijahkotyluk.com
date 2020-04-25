01c33bcd8b220d735b105296b1c73cf36ed25525
Elijah Kotyluk <elijah@elijahkotyluk.com>
2020-04-24 23:56:35 -0700

    feat(store): create vuex store
    Use octokit to pull my gh public events.

---
+++ store/index.js
@@ -0,0 +1,36 @@
+// Imports
+import { Octokit } from '@octokit/rest'
+
+// Vars
+const octokit = new Octokit({
+  token: process.env.GITHUB_API_TOKEN
+})
+
+// State
+export const state = () => ({
+  publicEvents: {}
+})
+
+// Actions
+export const actions = {
+  async getPublicEvents ({ commit }, payload) {
+    let events
+
+    try {
+      events = await octokit.activity.listPublicEventsForUser(payload)
+    } catch (e) {
+      console.log('[ERR]: ', e)
+    }
+
+    commit('setPublicEvents', events)
+  }
+}
+
+// Mutations
+export const mutations = {
+  setPublicEvents (state, payload) {
+    state.publicEvents = payload
+  }
+}
+
+// Getters
